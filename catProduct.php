c46226117cd838ff40ef8eaa45ef08c7606e36c0
Marcus <marcus.xuan@Hotmail.com>
2025-01-31 09:11:52 +0800

    update for OFS and offer

--- catProduct.php
+++ catProduct.php
@@ -1,59 +1,67 @@
-﻿<?php 
+﻿<?php
 session_start(); // Detect the current session
 include("header.php"); // Include the Page Layout header
 ?>
 <!-- Create a container, 60% width of viewport -->
 <div style='width:60%; margin:auto;'>
-<!-- Display Page Header - Category's name is read 
-     from the query string passed from previous page -->
-<div class="row" style="padding:5px">
-	<div class="col-12">
-		<span class="page-title"><?php echo "$_GET[catName]"; ?></span>
-	</div>
-</div>
+    <!-- Display Page Header - Category's name is read
+         from the query string passed from previous page -->
+    <div class="row" style="padding:5px">
+        <div class="col-12">
+            <span class="page-title"><?php echo "$_GET[catName]"; ?></span>
+        </div>
+    </div>
 
-<?php 
-// Include the PHP file that establishes database connection handle: $conn
-include_once("mysql_conn.php");
+    <?php
+    // Include the PHP file that establishes database connection handle: $conn
+    include_once("mysql_conn.php");
 
-// To Do:  Starting ....
-$cid=$_GET["cid"]; //Read category ID from query string
-// Form SQL to retrieve list of products asscociated to the Category ID
-$qry = "SELECT p.ProductID, p.ProductTitle, p.ProductImage, p.Price, p.Quantity
-		FROM CatProduct cp INNER JOIN product p ON cp.ProductID=p.ProductID
-		WHERE cp.CategoryID=?" ;
-$stmt = $conn->prepare($qry);
-$stmt->bind_param("i", $cid); // i - integer
-$stmt->execute();
-$result = $stmt->get_result();
-$stmt->close();
+    // To Do:  Starting ....
+    $cid=$_GET["cid"]; //Read category ID from query string
+    // Form SQL to retrieve list of products asscociated to the Category ID
+    $qry = "SELECT p.ProductID, p.ProductTitle, p.ProductImage, p.Price, p.Quantity,
+                   p.offered, p.OfferStartDate, p.OfferEndDate, p.OfferedPrice
+            FROM CatProduct cp INNER JOIN product p ON cp.ProductID=p.ProductID
+            WHERE cp.CategoryID=?" ;
+    $stmt = $conn->prepare($qry);
+    $stmt->bind_param("i", $cid); // i - integer
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
 
-// Display each product in a row
-while ($row = $result->fetch_array()) {
-	echo "<div class='row' style='padding:5px'>"; // Start a new row
+    // Display each product in a row
+    while ($row = $result->fetch_array()) {
+        echo "<div class='row' style='padding:5px'>"; // Start a new row
 
-	// Left column - display a text link showing the product's name, 
-	//				 display the selling price in red in a new paragraph
-	$product = "productDetails.php?pid=$row[ProductID]";
-	$formattedPrice = number_format($row["Price"], 2);
-	echo "<div class='col-8'>"; // 67% of row width 
-	echo "<p><a href=$product>$row[ProductTitle]</a></p>";
-	echo "Price:<span style='font-weight: bold; color: red;'>
-	 	  S$ $formattedPrice</span>";
-	echo "</div>";
+        // Left column - display a text link showing the product's name,
+        //				 display the selling price in red in a new paragraph
+        $product = "productDetails.php?pid=$row[ProductID]";
+        $formattedPrice = number_format($row["Price"], 2);
+        echo "<div class='col-8'>"; // 67% of row width
+        echo "<p><a href=$product>$row[ProductTitle]</a>";
 
-	// Right column - display the product's image
-	$img = "./Images/products/$row[ProductImage]";
-	echo "<div class='col-4'>"; // 33% of row width
-	echo "<img src='$img' />";
-	echo "</div>";
+          // On Offer Logic
+          if ($row['offered'] == 1 && $row['OfferStartDate'] <= date('Y-m-d') && $row['OfferEndDate'] >= date('Y-m-d')):
+                 echo "<span class='on-offer'>On Offer</span>";
+          endif;
 
-	echo "</div>"; // End of a row
-}
+          echo "</p>";
+        echo "Price:<span style='font-weight: bold; color: red;'>
+              S$ $formattedPrice</span>";
+        echo "</div>";
 
-// To Do:  Ending ....
+        // Right column - display the product's image
+        $img = "./Images/products/$row[ProductImage]";
+        echo "<div class='col-4'>"; // 33% of row width
+        echo "<img src='$img' />";
+        echo "</div>";
 
-$conn->close(); // Close database connnection
-echo "</div>"; // End of container
-include("footer.php"); // Include the Page Layout footer
-?>
+        echo "</div>"; // End of a row
+    }
+
+    // To Do:  Ending ....
+
+    $conn->close(); // Close database connnection
+    echo "</div>"; // End of container
+    include("footer.php"); // Include the Page Layout footer
+    ?>
--- productDetails.php
+++ productDetails.php
@@ -1,24 +1,26 @@
-﻿<?php 
+﻿<?php
 session_start(); // Detect the current session
 include("header.php"); // Include the Page Layout header
 ?>
 <!-- Create a container, 90% width of viewport -->
 <div style='width:90%; margin:auto;'>
 
-<?php 
+<?php
 $pid=$_GET["pid"]; // Read Product ID from query string
-
+if(!isset($pid)){
+   die("Error: Missing product id");
+}
 // Include the PHP file that establishes database connection handle: $conn
-include_once("mysql_conn.php"); 
-$qry = "SELECT * from product where ProductID=?";
+include_once("mysql_conn.php");
+$qry = "SELECT *, Quantity, offered, OfferStartDate, OfferEndDate, OfferedPrice,Price from product where ProductID=?"; // Changed this to include Quantity
 $stmt = $conn->prepare($qry);
-$stmt->bind_param("i", $pid); 	// "i" - integer 
+$stmt->bind_param("i", $pid); 	// "i" - integer
 $stmt->execute();
 $result = $stmt->get_result();
 $stmt->close();
 
 // To Do 1:  Display Product information. Starting ....
-while ($row = $result->fetch_array()) {
+while ($row = $result->fetch_array()) { // start while loop {
     // Display Page Header
     // Product's name is read from the "ProductTitle" column of "product" table.
     echo "<div class='row' >";
@@ -52,25 +54,51 @@ while ($row = $result->fetch_array()) {
     echo "<div class='col-sm-3' style='vertical-align:top; padding:5px'>";
     echo "<p><img src=$img /></p>";
 
-    // Right column - display the product's price 
+    // Right column - display the product's price
     $formattedPrice = number_format($row["Price"], 2);
-    echo "Price:<span style='font-weight:bold; color:red;'>
-         S$ $formattedPrice</span>";
-}
+      $offerStartDate = strtotime($row['OfferStartDate']);
+      $offerEndDate = strtotime($row['OfferEndDate']);
+      $today = strtotime(date('Y-m-d'));
 
-// To Do 1:  Ending ....
 
-// To Do 2:  Create a Form for adding the product to shopping cart. Starting ....
-echo "<form action='cartFunctions.php' method='post'>";
-echo "<input type='hidden' name='action' value='add' />";
-echo "<input type='hidden' name='product_id' value='$pid' />";
-echo "Quantity: <input type='number' name='quantity' value='1'
-                min='1' max='10' style='width:40px' required />";
-echo "<button type='submit'>Add to Cart</button>";
-echo "</form>";
+    // Start of offer check
+    echo  "<p>";
+    if ($row['offered'] == 1) { // start if
+        echo "<span class='on-offer'>On Offer</span>";
+        echo "<p>Price Before Offer: <strike>S$" . number_format($row['Price'], 2) . "</strike></p>";
+        echo "<p>Price: <span class='price'>S$" . number_format($row['OfferedPrice'], 2) . "</span></p>";
+    } else { // else statement for offer check
+        echo "Price: <span class='price'>S$" . $formattedPrice . "</span>";
+    } // end if for offer check
+    echo  "</p>";
+    
+    // Start of out of stock check
+    if($row['Quantity'] > 0) { // start if
+        // To Do 2:  Create a Form for adding the product to shopping cart. Starting ....
+        ?>
+        <form action='cartFunctions.php' method='post'>
+            <input type='hidden' name='action' value='add' />
+            <input type='hidden' name='product_id' value='<?php echo $pid; ?>' />
+            Quantity: <input type='number' name='quantity' value='1'
+                            min='1' max='10' style='width:40px' required />
+            <button type='submit'>Add to Cart</button>
+        </form>
+        <?php
+    // To Do 2:  Ending ....
+     } else { // else statement for out of stock check
+         ?>
+        <span class='out-of-stock'>Out of Stock</span>
+        <button disabled>Add to Cart</button>
+    <?php
+      } // end if for out of stock check
+
+    // End of out of stock check
+
+
+} // end of while loop
+
 echo "</div>"; // End of right column
 echo "</div>"; // End of Row
-// To Do 2:  Ending ....
 
 $conn->close(); // Close database connnection
 echo "</div>"; // End of container
