f8f4b0b5521e7e7cd29fee79d52232629c4064e9
Haneef03 <s10223481@connect.np.edu.sg>
2025-02-09 17:53:21 +0800

    fixed trash icon

--- shoppingCart.php
+++ shoppingCart.php
@@ -3,7 +3,7 @@
 // Current session is detected in "cartFunctions.php, hence need not start session here.
 include_once("cartFunctions.php");
 include("header.php"); // Include the Page Layout header
-
+$itemCount = 0; // Initialize the item count
 if (! isset($_SESSION["ShopperID"])) { // Check if user logged in 
 	// redirect to login page if the session variable shopperid is not set
 	header ("Location: login.php");
